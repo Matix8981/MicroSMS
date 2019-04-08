140fe55b46abd3aa56323201b9e750d44c07039d
Mateusz Woźnowski <mateusz8981@gmail.com>
2019-04-08 13:37:31 +0200

    Update sms.php

--- src/sms.php
+++ src/sms.php
@@ -3,7 +3,7 @@
      * @package MicroSMS
      * @author Mateusz Woźnowski <mateusz8981@gmail.com>
      * @link https://github.com/Matix8981/MicroSMS
-     * @license   https://opensource.org/licenses/MIT The MIT License
+     * @license https://opensource.org/licenses/MIT The MIT License
      * @version 1.0.0
      */
     namespace Matix8981\Payments\MicroSMS;
@@ -17,11 +17,13 @@
         {
             $this->clientID = $clientID;
             $this->serviceID = $serviceID;
+            $this->response = null;
         }
 
         /**
          * @param $code - SMS code to check
          * @param $number - The number to which the SMS was sent
+         * @return array result
          */
         public function ValidateSingle($code, $number)
         {
@@ -35,45 +37,46 @@
                         {
                             $curl = curl_init();
                             curl_setopt_array($curl, array(
-                                CURLOPT_URL => "http://microsms.pl/api/v2/index.php?userid={$this->clientID}&serviceid={$this->serviceID}&number={$number}&code={$code}",
+                                CURLOPT_URL => "https://microsms.pl/api/v2/index.php?userid={$this->clientID}&serviceid={$this->serviceID}&number={$number}&code={$code}",
                                 CURLOPT_RETURNTRANSFER => "1",
                             ));
 
-                            $data = json_decode(curl_exec($curl));
-                            if(!is_null($data) && is_object($data))
+                            $this->response = json_decode(curl_exec($curl), true);
+                            if(!is_null($this->response) && is_array($this->response))
                             {
-                                return $data;
+                                return $this->response;
                             }
                             else
                             {
-                                throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Błąd podczas nawiązywania połączenia!");
+                                throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Błąd podczas nawiązywania połączenia!");
                             }
 
                             curl_close($curl);
                         }
                         else
                         {
-                            throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> code jest niepoprawny!");
+                            throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> code jest niepoprawny!");
                         }
                     }
                     else
                     {
-                        throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> number nie jest liczbą bądź jest pusty!");
+                        throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> number nie jest liczbą bądź jest pusty!");
                     }
                 }
                 else
                 {
-                    throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> clientID lub serviceID nie są liczbami!");
+                    throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> clientID lub serviceID nie są liczbami!");
                 }
             }
             else
             {
-                throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Proszę wprowadzić clientID oraz serviceID!");
+                throw new \Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Proszę wprowadzić clientID oraz serviceID!");
             }
         }
 
         /**
          * @param $code - SMS code to check
+         * @return array result
          */
         public function ValidateMulti($code)
         {
@@ -85,35 +88,60 @@
                     {
                         $curl = curl_init();
                         curl_setopt_array($curl, array(
-                            CURLOPT_URL => "http://microsms.pl/api/v2/multi.php?userid={$this->clientID}&serviceid={$this->serviceID}&code={$code}",
+                            CURLOPT_URL => "https://microsms.pl/api/v2/multi.php?userid={$this->clientID}&serviceid={$this->serviceID}&code=${code}",
                             CURLOPT_RETURNTRANSFER => "1",
                         ));
 
-                        $data = json_decode(curl_exec($curl));
-                        if(!is_null($data) && is_object($data))
+                        $this->response = json_decode(curl_exec($curl), true);
+                        if(!is_null($this->response) && is_array($this->response))
                         {
-                            return $data;
+                            return $this->response;
                         }
                         else
                         {
-                            throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Błąd podczas nawiązywania połączenia!");
+                            throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Błąd podczas nawiązywania połączenia!");
                         }
-
                         curl_close($curl);
                     }
                     else
                     {
-                        throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> code jest niepoprawny!");
+                        throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> code jest niepoprawny!");
                     }
                 }
                 else
                 {
-                    throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> clientID lub serviceID nie są liczbami!");
+                    throw new \RuntimeException("<strong>Matix8981\Payments\MicroSMS\SMS</strong> clientID lub serviceID nie są liczbami!");
                 }
             }
             else
             {
-                throw new Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Proszę wprowadzić clientID oraz serviceID!");
+                throw new \Exception("<strong>Matix8981\Payments\MicroSMS\SMS</strong> Proszę wprowadzić clientID oraz serviceID!");
+            }
+        }
+
+        /**
+         * @return array result
+         */
+        public function ValidateResult()
+        {
+            if(!is_null($this->response) && is_array($this->response) && $this->response["data"]["status"] == 1)
+            {
+                return array(
+                    "status" => true,
+                    "used" => false,
+                    "service" => $this->response["data"]["service"],
+                    "number" => $this->response["data"]["number"],
+                    "phone" => $this->response["data"]["phone"],
+                    "reply" => $this->response["data"]["reply"],
+                );
+            }
+            elseif($this->response["connect"] == true && $this->response["data"]["status"] == 0 && $this->response["data"]["used"] == 1)
+            {
+                return array("status" => false, "used" => true);
+            }
+            else
+            {
+                return array("status" => false, "used" => false);
             }
         }
     }
